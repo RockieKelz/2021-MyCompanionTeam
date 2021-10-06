192c801e66ff09f5b5101f03adc893e50e0bd49b
whorton9516 <samwhorton@gmail.com>
2021-10-06 12:53:15 -0400

    Initial Commit - Added Logs Folder    - Added BaseLog    - Added JournalLog

---
+++ Codebase/Logs/src/com/company/JournalLog.java
@@ -0,0 +1,30 @@
+
+/*
+    Created by: Sam Whorton
+    Date Created: 10/6/2021
+    Last Modified: 10/6/2021
+ */
+
+package com.company;
+
+public class JournalLog extends BaseLog {
+
+    // Properties
+        //TODO: Add the appropriate properties to the Journal entries
+        //- does the entry need to be saved as a single string or as multiple?
+
+    /// METHODS ///
+
+    // Constructor
+
+    JournalLog(){
+        this.SetType(LogType.JOURNAL);
+    }
+
+    // Accessors
+
+
+    // Mutators
+
+
+}
