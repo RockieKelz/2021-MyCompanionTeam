a5fdf175ef70fb7490f15066bcbc5b0b9b3bf2da
Amber Xiong <ADXiong@student.fullsail.edu>
2021-10-25 15:28:00 -0500

    Created Journal Activity and Check-Up fragment

---
+++ Codebase/NavDrawer/app/src/main/java/com/example/navigationaldrawertutorial/JournalActivity.java
@@ -0,0 +1,16 @@
+package com.example.navigationaldrawertutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal);
+
+
+    }
+}
