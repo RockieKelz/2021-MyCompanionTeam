aeb0245392c9d912644eea4f899e5f9f8a94e9ec
whorton9516 <samwhorton@gmail.com>
2021-11-03 13:59:39 -0400

    Added date parameter to Log.java and method to override an entries date

--- Codebase/MyCompanion/Logs/src/main/java/com/example/logs/Log.java
+++ Codebase/MyCompanion/Logs/src/main/java/com/example/logs/Log.java
@@ -6,12 +6,14 @@
  */
 
 package com.example.logs;
+import java.util.Date;
 import java.util.Vector;
 
 public class Log {
 
     // Properties
     private Vector<BaseEntry> Entries;
+    private Date date;
 
     // METHODS //
 
@@ -20,17 +22,26 @@ public class Log {
     // Copy Constructor
     Log(Log log){
         SetEntries(log.GetEntries());
+        date = log.GetDate();
     }
 
 
     // Accessors
     public Vector<BaseEntry> GetEntries() { return Entries; }
+    public Date GetDate() { return date; }
 
     // Mutators
     public void SetEntries(Vector<BaseEntry> _entries) { Entries = _entries; }
+    public void SetDate(Date _date) { date = _date; }
 
 
-    public void AddEntry(BaseEntry entry) { Entries.add(entry); }
+    public void AddEntry(BaseEntry entry) {
+        if(entry.GetDate() == date) {
+            Entries.add(entry);
+        } else {
+            System.out.println("Entry is from a different date, cannot add to current Log.");
+        }
+    }
 
     public void RemoveEntry(BaseEntry entry) {
         if(Entries.contains(entry)){
@@ -39,6 +50,10 @@ public class Log {
         } else {
             System.out.println( "Entry not found, nothing to remove.");
         }
+    }
+
+    public void DateOverride(BaseEntry entry) {     // Gives ability to override an entries date to match the current log
+        entry.SetDate(date);
     }
 
 
