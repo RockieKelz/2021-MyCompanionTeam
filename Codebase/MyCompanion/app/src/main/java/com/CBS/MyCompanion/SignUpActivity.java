e4c8c35c8a2e93ef84843f697de40e2d36c284de
whorton9516 <samwhorton@gmail.com>
2021-11-11 12:54:12 -0500

    Added functionality to SignUpActivity.java

--- Codebase/MyCompanion/app/src/main/java/com/CBS/MyCompanion/SignUpActivity.java
+++ Codebase/MyCompanion/app/src/main/java/com/CBS/MyCompanion/SignUpActivity.java
@@ -1,22 +1,31 @@
 package com.CBS.MyCompanion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText emailEditText, passwordEditText;
     private Button loginButton, registerButton;
     private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +36,8 @@ public class SignUpActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.activity_main_passwordEditText);
         loginButton = findViewById(R.id.activity_main_loginButton);
         registerButton = findViewById(R.id.activity_main_registerButton);
+        progressBar = findViewById(R.id.activity_main_progressBar);
         mAuth = FirebaseAuth.getInstance();
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,14 +50,75 @@ public class SignUpActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(emailEditText.getText().length() > 0 && passwordEditText.getText().length() > 0) {
-                    String toastMessage = "Username: " + emailEditText.getText() + ", Password: " + passwordEditText.getText();
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-                } else {
-                    String toastMessage = "Username or Password has not been entered";
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-                }
+                registerNewUser();
             }
         });
     }
+
+    private void registerNewUser() {
+
+        // show the visibility of progress bar to show loading
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = emailEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter email!!",
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter password!!",
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        // create new user or register new user
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Registration successful!",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+
+                            // hide the progress bar
+                            progressBar.setVisibility(View.GONE);
+
+                            // if the user created intent to login activity
+                            Intent intent
+                                    = new Intent(SignUpActivity.this,
+                                    LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+
+                            // Registration failed
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Registration failed!!"
+                                            + " Please try again later",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
 }
