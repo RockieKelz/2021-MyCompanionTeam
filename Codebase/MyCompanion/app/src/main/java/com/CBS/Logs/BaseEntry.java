d8e8bf43ff02056fd8939e18af7766697cc91021
Amber Xiong <ADXiong@student.fullsail.edu>
2021-11-12 20:39:39 -0600

    Added margin around checkup layout page and 2 emotions to Base Entry Log.

--- Codebase/MyCompanion/app/src/main/java/com/CBS/Logs/BaseEntry.java
+++ Codebase/MyCompanion/app/src/main/java/com/CBS/Logs/BaseEntry.java
@@ -9,7 +9,7 @@ package com.CBS.Logs;
 import java.util.Date;
 
 enum EntryType { JOURNAL, CHECKUP }
-enum Emotions {SAD, HAPPY, ANXIOUS, CALM, ANGRY, EXCITED}
+enum Emotions {SAD, HAPPY, ANXIOUS, CALM, ANGRY, EXCITED, WITHDRAWN, FEARFUL}
 
 abstract class BaseEntry {
 
