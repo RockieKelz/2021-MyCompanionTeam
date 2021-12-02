cd0fdb3aa962c4fe607fb61f36510afbecb9aa50
Racquel McCall <RRMcCall@student.fullsail.edu>
2021-12-01 20:48:15 -0800

    Added ability to reset password and sign up navigation to LoginActivity.java Added background color to Login and SignUp screens

--- Codebase/MyCompanion/app/src/main/java/com/CBS/MyCompanion/LoginActivity.java
+++ Codebase/MyCompanion/app/src/main/java/com/CBS/MyCompanion/LoginActivity.java
@@ -1,31 +1,36 @@
 package com.CBS.MyCompanion;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.CBS.MyCompanion.Data.UserAccount;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailEditText, passwordEditText;
-    private Button loginButton;
+    private Button loginButton, registerButton, resetButton;
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
     private FirebaseFirestore database;
@@ -42,14 +47,27 @@ public class LoginActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.activity_login_emailEditText);
         passwordEditText = findViewById(R.id.activity_login_passwordEditText);
         loginButton = findViewById(R.id.activity_login_loginButton);
+        registerButton = findViewById(R.id.activity_login_registerButton);
+        resetButton = findViewById(R.id.activity_login_resetButton);
         progressBar = findViewById(R.id.activity_login_progressBar);
-
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 loginUserAccount();
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent s = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(s);
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
             }
         });
     }
@@ -109,5 +127,76 @@ public class LoginActivity extends AppCompatActivity {
                                 }
                             }
                         });
+    }
+    @SuppressLint("RestrictedApi")
+    public void resetPassword()
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+        LinearLayout linearLayout=new LinearLayout(this);
+        final TextView email = new TextView(this);
+        final EditText recoveryEmail= new EditText(this);
+
+        // write the email using which you registered
+        email.setText(R.string.email);
+        email.setTextColor(getResources().getColor(R.color.black));
+        email.setTextSize(16);
+        recoveryEmail.setMinEms(12);
+        recoveryEmail.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        linearLayout.addView(email);
+        linearLayout.addView(recoveryEmail);
+        linearLayout.setGravity(Gravity.CENTER);
+        linearLayout.setPadding(15,40,15,15);
+
+        builder.setView(linearLayout,(15), (40), (15), (15) );
+
+        // Click on Recover and a email will be sent to your registered email id
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String Email=recoveryEmail.getText().toString().trim();
+                recoverPassword(Email);
+            }
+        });
+
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+    public void recoverPassword(String resetEmail)
+    {
+        ProgressDialog loadingBar=new ProgressDialog(this);
+        loadingBar.setMessage("Sending Email....");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        // calling sendPasswordResetEmail
+        // open your email and write the new
+        // password and then you can login
+        mAuth.sendPasswordResetEmail(resetEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                loadingBar.dismiss();
+                if(task.isSuccessful())
+                {
+                    // if isSuccessful then done message will be shown
+                    // and you can change the password
+                    Toast.makeText(LoginActivity.this,"Done sent",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Error Occurred",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(LoginActivity.this,"Error Failed",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
